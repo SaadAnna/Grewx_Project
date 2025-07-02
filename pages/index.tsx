49d9f5fce78bf5d171547bc976589fa46c32b90e
Saad Anna <saadannadev@gmail.com>
2025-07-02 11:54:54 +0200

    Start the project and finish the navbar section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Navbar from "./Sections/Navbar";
+import Hero from "./Sections/Hero";
+import Company from "./Sections/Company";
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Company />
     </>
   );
 }
