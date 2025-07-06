45ad03436aab0b88b2605ba7fa2b00c5ff79b771
Saad Anna <saadannadev@gmail.com>
2025-07-06 13:29:21 +0200

    Start the services Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./Sections/Navbar";
 import Hero from "./Sections/Hero";
 import Company from "./Sections/Company";
+import Advantages from "./Sections/Advantages";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
       <Company />
+      <Advantages />
     </>
   );
 }
