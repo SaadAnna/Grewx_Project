05ea07fa99607627efa40e585e93c4c4b185f555
Saad Anna <saadannadev@gmail.com>
2025-07-07 12:45:13 +0200

    Start and finish the analytics section

---
+++ pages/Sections/Analytics.tsx
@@ -0,0 +1,30 @@
+export default function Analytics() {
+  return (
+    <section className="py-24">
+      <div className="px-4 lg:max-w-5xl max-w-4xl mx-auto">
+        <div className="mx-auto w-auto h-fit p-2 lg:p-10 bg-gray-950 rounded-2xl  ">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-52 gap-10 items-center justify-center">
+          <div className="flex flex-col text-center gap-3">
+            <h1 className="text-white font-medium text-4xl ">10+</h1>
+            <span className="text-white/70 text-sm">
+              Years Years Experience
+            </span>
+          </div>
+          <div className="flex flex-col text-center gap-3">
+            <h1 className="text-white font-medium text-4xl ">555+</h1>
+            <span className="text-white/70 text-sm">
+              Stisfied Client
+            </span>
+          </div>
+          <div className="flex flex-col text-center gap-3">
+            <h1 className="text-white font-medium text-4xl ">10+</h1>
+            <span className="text-white/70 text-sm">
+              Award Archieved
+            </span>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
