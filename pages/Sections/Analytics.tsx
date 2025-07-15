9888dd1b70297cbc103b1ae4a3e62d69e724f510
Saad Anna <saadannadev@gmail.com>
2025-07-15 23:08:05 +0200

    Add some new content in the Anzlytics section in the project

--- pages/Sections/Analytics.tsx
+++ pages/Sections/Analytics.tsx
@@ -7,19 +7,19 @@ export default function Analytics() {
           <div className="flex flex-col text-center gap-3">
             <h1 className="text-white font-medium text-4xl ">10+</h1>
             <span className="text-white/70 text-sm">
-              Years Years Experience
+              Years Years Experience.
             </span>
           </div>
           <div className="flex flex-col text-center gap-3">
             <h1 className="text-white font-medium text-4xl ">555+</h1>
             <span className="text-white/70 text-sm">
-              Stisfied Client
+              Stisfied Client.
             </span>
           </div>
           <div className="flex flex-col text-center gap-3">
             <h1 className="text-white font-medium text-4xl ">10+</h1>
             <span className="text-white/70 text-sm">
-              Award Archieved
+              Award Archieved.
             </span>
           </div>
           </div>
